8d18e1428f8aa75e5d6a322a593875c4134e3eb5
harshrao26 <harshurao058@gmail.com>
2024-04-11 12:15:22 +0530

    Create Access & Refresh Token

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,3 +1,4 @@
+import env from 'dotenv'
 import mongoose, { Schema } from 'mongoose'
 import jwt from 'jsonwebtoken'
 import bcrypt from 'bcrypt'
@@ -44,5 +45,28 @@ userSchema.pre("save", async function (next) {
 })
 userSchema.methods.isPasswordCorrect = async function (password) {
     return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateAccessToken = function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+            username: this.username,
+            email: this.email
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRES_IN
+        })
+}
+userSchema.methods.generateRefreshToken = function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRES_IN
+        })
 }
 export const User = mongoose.model('User', userSchema) 
