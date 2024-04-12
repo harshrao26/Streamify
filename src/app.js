b6caf7445582adfb88ee1c572f15ba22906a216e
harshrao26 <harshurao058@gmail.com>
2024-04-12 23:54:55 +0530

    All SetUp Configured

--- src/app.js
+++ src/app.js
@@ -21,4 +21,13 @@ app.use(express.urlencoded({
 app.use(express.static('public'))
 app.use(cookieParser())
 
+
+//Routes Import
+import userRoutes from './routes/user.routes.js';
+app.use('/api/v1/users', userRoutes)
+
+
+
+
+
 export default app;
--- src/utils/apiError.js
+++ src/utils/apiError.js
@@ -1,25 +1,17 @@
 class ApiError extends Error {
-    constructor(
-        statusCode,
-        message = "Something Went Wrong",
-        errors = [],
-        stack = ""
-
-
-    ) {
-        super(message)
-        this.statusCode = statusCode
-        this.data = null
-        this.message = message
-        this.errors = errors
-        this.success = false
+    constructor(statusCode, message = "Something Went Wrong", errors = [], stack = "") {
+        super(message);
+        this.statusCode = statusCode;
+        this.data = null;
+        this.errors = errors;
+        this.success = false;
 
         if (stack) {
-            this.stack = stack
-        }
-        else {
-            Error.captureStackTrace(this, this.constructor)
+            this.stack = stack;
+        } else {
+            Error.captureStackTrace(this, this.constructor);
         }
     }
 }
-export default ApiError
+
+export default ApiError;
