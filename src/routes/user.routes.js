2cdf64303ede0762baf9908b5f7a4383b6a28783
harshrao26 <harshurao058@gmail.com>
2024-04-14 22:41:37 +0530

    Cover Img, Avatar Uploading Done

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -7,7 +7,7 @@ const router = Router();
 router.route('/register').post(
     upload.fields([
         { name: "avatar", maxCount: 1 },
-        { name: "coverImage", maxCount: 1 }
+        { name: "coverImg", maxCount: 1 }
     ]),
     registerUser
 );
